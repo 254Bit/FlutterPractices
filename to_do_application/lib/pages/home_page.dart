16a061a640d11dd0692325739d7cf028bc8933af
254Bit <oliviaayora@gmail.com>
2024-07-25 21:40:31 +0300

    Error bug fix

--- to_do_application/lib/pages/home_page.dart
+++ to_do_application/lib/pages/home_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:to_do_application/data/database.dart';
 import 'package:to_do_application/utilities/dialogue_box.dart';
 import 'package:to_do_application/utilities/todo_tile.dart';
-import 'package:to_do_application/utilities/my_buttons.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,7 +24,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   //Text Controller
-  final _controller = TextEditingController;
+  get _controller => TextEditingController;
 
   // Check box tapped or not
   void checkBoxChanged(bool? value, int index) {
